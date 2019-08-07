d35711aa414d15c5db68070fbcba0ed8cdb75edf
Kim-Taesu <dkdldoafotn@naver.com>
2019-08-08 00:12:23 +0900

    update spanning_tree bj_4195

--- spanning_tree/bj_4195.py
+++ spanning_tree/bj_4195.py
@@ -8,40 +8,45 @@ INF = sys.maxsize
 input = sys.stdin.readline
 
 
-def getParent(node, f1):
-    # print(node,f1)
-    if node[f1][0] == f1: return f1
-    result = getParent(node, node[f1][0])
-    return result
+def getParent(num):
+    if node[num] == num: return num
+    node[num] = getParent(node[num])
+    return node[num]
 
 
-def union(node, f1, f2):
-    f1 = getParent(node, f1)
-    f2 = getParent(node, f2)
+def union(f1, f2):
+    f1 = getParent(f1)
+    f2 = getParent(f2)
 
-    if f1 > f2:
-        # node[f1] = f2
-        node[f1] = (f2, node[f2][1] + node[f1][1])
-        node[f2] = (f2, node[f1][1])
-        return f2
-    else:
-        # node[f2] = f1
-        node[f1] = (f1, node[f1][1] + node[f2][1])
-        node[f2] = (f1, node[f1][1])
-        return f1
+    if f1 != f2:
+        node[f1] = f2
+        nodeWeight[f2] += nodeWeight[f1]
+        nodeWeight[f1] = 1
+
+    return nodeWeight[f2]
 
 
 for _ in range(int(input().strip())):
     r = int(input().strip())
 
-    node = {}
+    node = []
+    nodeWeight = []
+
+    for i in range(r * 2):
+        node.append(i)
+        nodeWeight.append(1)
+    dict = {}
+    count = 0
     for _ in range(r):
         f1, f2 = input().strip().split()
-        if f1 not in node:
-            node[f1] = (f1, 1)
-        if f2 not in node:
-            node[f2] = (f2, 1)
-        par = union(node, f1, f2)
+        if not f1 in dict:
+            dict[f1] = count
+            count += 1
+        a = dict[f1]
 
-        # print(node)
-        print(node[par][1])
+        if not f2 in dict:
+            dict[f2] = count
+            count += 1
+        b = dict[f2]
+
+        print(union(a, b))
