939d7db18bdded20e3b4e4561a5a3ed18b5589f7
Kim-Taesu <dkdldoafotn@naver.com>
2019-08-16 00:51:32 +0900

    update back_tracking and greedy

--- back_tracking/bj_2661.py
+++ back_tracking/bj_2661.py
@@ -3,18 +3,33 @@ import sys
 sys.setrecursionlimit(10 ** 6)
 
 N = int(input())
-arr = [1, 2, 3]
+arr = ['1', '2', '3']
+flag=False
 
+def check(s):
+    length=len(s)
+    limit=length//2
+    start=length-1
+    end=length
 
-def dfs(first, second, result):
+    for i in range(1,limit+1):
+        if s[start-i:end-i] == s[start:end]: return False
+        start-=1
 
-    
+    return True
 
+def dfs(s):
+    if len(s)==N:
+        print(s)
+        sys.exit(0)
+    else:
+        for i in arr:
+            if(check(s+i)):
+                dfs(s+i)
     pass
 
 
 for i in arr:
-    result = []
-    result.append(i)
-    dfs(-1, i, result)
-    result.pop()
+    dfs(i)
+
+
---
+++ greedy/bj_1931.py
@@ -0,0 +1,20 @@
+import sys
+sys.setrecursionlimit(10**6)
+import heapq
+input = sys.stdin.readline
+N=int(input().strip())
+conf=[]
+for _ in range(N):
+    start,end=map(int,input().strip().split())
+    conf.append((start,end))
+conf=sorted(conf,key=lambda time:time[0])
+conf=sorted(conf,key=lambda time:time[1])
+def find(conf):
+    count=0
+    time=0
+    for i in conf:
+        if i[0] >= time:
+            time=i[1]
+            count+=1
+    return count
+print(find(conf))
