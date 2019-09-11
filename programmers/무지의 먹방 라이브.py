4f1eac1a5ba42c158077a182ef2753c43d199e6c
Kim-Taesu <dkdldoafotn@naver.com>
2019-09-11 15:53:39 +0900

    update 무지의 먹방 라이브

---
+++ programmers/무지의 먹방 라이브.py
@@ -0,0 +1,42 @@
+def solution(food_times, k):
+    answer = -1
+    index = []
+    l = len(food_times)
+    i = 0
+
+    # (food, index)
+    for f in food_times:
+        index.append((f, i))
+        i += 1
+
+    index.sort()
+    # print(index)
+
+    before = 0
+
+    for i in index:
+        tmp = (i[0] - before) * l
+        before =i[0]
+        # print(k,tmp,before)
+        # print(food_times)
+
+        if tmp <= k:
+            k -= tmp
+
+        else:
+            k%=l
+            cnt=-1
+            for f in range(len(food_times)):
+                if food_times[f] == ('n','n'):
+                    continue
+                else:
+                    cnt+=1
+                if cnt==k:
+                    answer=f+1
+                    break
+            break
+
+        food_times[i[1]]=('n','n')
+        l -= 1
+
+    return answer
--- programmers/후보키.py
+++ programmers/후보키.py
@@ -7,34 +7,29 @@ def solution(relation):
     indexs = []
     checkList = []
 
-    if len(relation) > 0:
-        colSize = len(relation[0])
-        rowSize = len(relation)
+    colSize = len(relation[0])
+    rowSize = len(relation)
 
-        # 모든 컬럼의 조합 구하기 (Set형태)
-        for i in range(1, colSize + 1):
-            # append는 런타임에러가 뜸 append와 extend 비교하여 알아둘 것
-            indexs.extend([set(k) for k in combinations([j for j in range(colSize)], i)])
+    for i in range(1, colSize + 1):
+        indexs.extend([set(k) for k in combinations([j for j in range(colSize)], i)])
 
-        for index in indexs:
-            check = set()
-            for r in range(rowSize):
-                tmp = ''
-                for i in index:
-                    tmp += relation[r][i]
-                check.add(tmp)
+    for index in indexs:
+        check = set()
+        for r in relation:
+            tmp = ''
+            for i in index:
+                tmp += r[i]
+            check.add(tmp)
+        if len(check) == rowSize: checkList.append(index)
+    print(checkList)
 
-            if len(check) == rowSize: checkList.append(index)
+    delSet = set()
 
-        delSet = set()
+    for comp1 in checkList:
+        for ind, comp2 in enumerate(checkList):
+            if comp1.issubset(comp2) and comp1 != comp2:
+                delSet.add(checkList.index(comp2))
 
-        for comp1 in checkList:
-            for ind, comp2 in enumerate(checkList):
-                if comp1.issubset(comp2) and comp1 != comp2:
-                    delSet.add(checkList.index(comp2))
+    print(delSet)
 
-        answer = len(checkList) - len(delSet)
-
-        print(checkList)
-
-        return answer
+    return len(checkList) - len(delSet)
