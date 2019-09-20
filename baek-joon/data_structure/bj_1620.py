830106d11632d5b6d3fe772ce134d2c41b6a169e
Kim-Taesu <dkdldoafotn@naver.com>
2019-09-20 10:02:44 +0900

    update baek joon 1620

---
+++ baek-joon/data_structure/bj_1620.py
@@ -0,0 +1,22 @@
+import sys
+
+sys.setrecursionlimit(10 ** 6)
+input = sys.stdin.readline
+
+N, M = map(int, input().strip().split())
+
+data = {}
+data2={}
+for i in range(N):
+    mon = input().strip()
+    data[mon] = i + 1
+
+line = list(data.keys())
+for i in range(M):
+    tmp = input().strip()
+    # 숫자
+    if tmp.isdecimal():
+        print(line[int(tmp)-1])
+    # 문자
+    else:
+        print(data[tmp])
