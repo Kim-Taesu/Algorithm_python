b16dbac343d487b6bcd033db4e9799c56cb9e4a9
Kim-Taesu <dkdldoafotn@naver.com>
2020-02-12 21:45:23 +0900

    add 사다리 조작

--- baek_joon/brute_force/bj_15684.py
+++ baek_joon/brute_force/bj_15684.py
@@ -1,19 +1,66 @@
-line = input().split(' ')
-n = int(line[0])
-m = int(line[1])
-h = int(line[2])
+import sys
 
-map = [[0 for i in range(n)] for i in range(m)]
+# N : 세로선 (열)
+# M : 가로선
+# H : 가로선 위치의 개수 (행)
 
-for i in map:
-    print(i)
+N, M, H = map(int, input().strip().split(' '))
+min_value = sys.maxsize
 
-for i in range(m):
-    line = input().split(' ')
-    aTmp = int(line[0]) - 1
-    bTmp = int(line[1]) - 1
-    map[aTmp][bTmp] = 1
-    # map[aTmp][bTmp + 1] = 1
+arr = [[0] * ((N * 2) - 1) for _ in range(H)]
 
-for i in map:
-    print(i)
+for _ in range(M):
+    a, b = map(int, input().strip().split(' '))
+    a -= 1
+    b *= 2
+    b -= 1
+    arr[a][b] = 1
+
+
+def find(cnt):
+    # print('선 횟수', cnt)
+    # for a in arr:
+    #     print(a)
+    # print()
+
+    for j in range(0, (N * 2) - 1, 2):
+        start = j
+
+        for i in range(H + 1):
+            if i == H:
+                if start == j:
+                    break
+                else:
+                    return
+            if start - 1 >= 0 and arr[i][start - 1] == 1:
+                start -= 2
+            elif start + 1 < 2 * N - 1 and arr[i][start + 1] == 1:
+                start += 2
+
+    global min_value
+    min_value = min(min_value, cnt)
+
+
+def dfs(cx, cy, cnt):
+    # 선 넘어서면
+    if cx < 0 or cx >= H or cy < 0 or cy >= 2 * N - 1:
+        return
+
+    # 선 3개 넘으면 return
+    if cnt > 3:
+        return
+
+    find(cnt)
+
+    for ni in range(cx, H):
+        for nj in range(1, (N * 2) - 1, 2):
+            if arr[ni][nj] == 0:
+                arr[ni][nj] = 1
+                dfs(ni, nj, cnt + 1)
+                arr[ni][nj] = 0
+
+
+if min_value == sys.maxsize:
+    dfs(0, 0, 0)
+
+print(min_value if min_value != sys.maxsize else -1)
